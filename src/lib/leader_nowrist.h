155712e21133b4a21695bc0ec753f9cb67096d83
amir-noohian <amirhossein76.noohian@gmail.com>
2026-05-04 10:08:46 -0600

    added changes

--- src/lib/leader_nowrist.h
+++ src/lib/leader_nowrist.h
@@ -99,7 +99,7 @@ class Leader : public barrett::systems::System {
             extTorque << 0.0, 0.0, 0,0, 0.0;
         }
 
-        sendJpMsg << wamJP, -1.57, 0.0, 1.57;
+        sendJpMsg << wamJP, 0.0, 0.0, 0.0;
         sendJvMsg << wamJV, 0.0, 0.0, 0.0;
         sendExtTorqueMsg << extTorque, 0.0, 0.0, 0.0;
 
@@ -197,6 +197,6 @@ class Leader : public barrett::systems::System {
 
 
 
-        return u1;
+        return u4;
     };
 };
